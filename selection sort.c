90079a3aeeebcf31bc19746a9c80ab3022d8554a
sohol1324 <50266496+sohol1324@users.noreply.github.com>
2019-05-25 21:36:58 +0900

    Add files via upload

---
+++ selection sort.c
@@ -0,0 +1,29 @@
+/*예제: 선택정렬
+  201914023 정우진*/
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void)
+{
+	int list[SIZE] = { 3, 2, 9, 7, 1, 4, 8, 0, 6, 5 };
+	int i, j, temp, least = 1;
+
+	for (i = 0; i < SIZE - 1; i++)
+	{
+		least = i;
+		for (j = i + 1; j < SIZE; j++)
+			if (list[j] < list[least])
+				least = j;
+
+		temp = list[i];
+		list[i] = list[least];
+		list[least] = temp;
+	}
+	for (i = 0; i < SIZE; i++)
+	{
+		printf("%d ", list[i]);
+	}
+	printf("\n원래의 배열 : 3 2 9 7 1 4 8 0 6 5\n");
+	return 0;
+}
