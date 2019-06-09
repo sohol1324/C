a7a4ae8c37b399fcdc5eb5c25f3ed950b8c6a5e1
sohol1324 <50266496+sohol1324@users.noreply.github.com>
2019-06-09 19:55:43 +0900

    Add files via upload

---
+++ 2chinput.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char fruits[3][20];
+	for (i = 0; i < 3; i++)
+	{
+		printf("과일 이름을 입력하시오: ", fruits[i]);
+		scanf("%s", fruits[i]);
+	}
+	for (i = 0; i < 3; i++)
+	{
+		printf("%d번째 과일: %s\n", i, fruits[i]);
+	}
+
+	return 0;
+}
