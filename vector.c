d584107ae4620bd34eea2ee6164f03b6c00804a5
sohol1324 <50266496+sohol1324@users.noreply.github.com>
2019-06-12 21:33:51 +0900

    Add files via upload

---
+++ vector.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct vector {
+	float x;
+	float y;
+};
+struct vector get_vector_sum(struct vector a, struct vector b);
+
+int main(void)
+{
+	struct vector a = { 2.0, 3.0 };
+	struct vector b = { 5.0, 6.0 };
+	struct vector sum;
+
+	sum = get_vector_sum(a, b);
+	printf("벡터의 합은 (%f, %f)입니다.\n", sum.x, sum.y);
+
+	return 0;
+}
+
+struct vector get_vector_sum(struct vector a, struct vector b)
+{
+	struct vector result;
+
+	result.x = a.x + b.x;
+	result.y = a.y + b.y;
+
+	return result;
+}
